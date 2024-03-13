6cd366c1163ae72a3fa58f65f9d976c59863546e
Martin Braun <martin.braun@mercatis.com>
2024-03-13 13:08:04 +0100

    connect to camera wifi network on demand.

--- app/TODO.md
+++ app/TODO.md
@@ -6,8 +6,11 @@ TODO: display video stream from rollei camera
     DONE: add configuration fragment to main activity
     
     next up:
-    TODO: connect to camera network 
-    TODO: display camer stream in preview
+    TODO: connect to camera network
+        DONE: use NetworkRequest api.
+        TODO: app wide preferences using SharedPreferences, look up an example and implement.
+
+    TODO: display camera stream in preview
     TODO: create an ssh tunnel from android to virtual machine (obsbox)
     TODO: publish stream to obsbox
 
@@ -18,3 +21,7 @@ TODO: display video stream from rollei camera
         - night vision?
 
 [1] https://developer.android.com/develop/ui/views/components/settings#java
+[2] https://developer.android.com/reference/android/net/wifi/package-summary
+[3] https://gist.github.com/liu7yong/f3765398b2ea5f30c986
+[4] https://stackoverflow.com/questions/53620234/android-wifimanager-enablenetwork-returning-false
+[5] https://developer.android.com/develop/connectivity/wifi/wifi-suggest
--- app/src/main/java/com/randomher0/CameraActivity.java
+++ app/src/main/java/com/randomher0/CameraActivity.java
@@ -1,61 +1,50 @@
 package com.randomher0;
 
 import static android.app.PendingIntent.getActivity;
+import static android.graphics.Insets.add;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.MacAddress;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkRequest;
+import android.net.NetworkSpecifier;
 import android.net.Uri;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiEnterpriseConfig;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.net.wifi.WifiNetworkSpecifier;
+import android.net.wifi.WifiNetworkSuggestion;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.annotation.NonNull;
+import androidx.preference.PreferenceManager;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.PatternMatcher;
 import android.view.View;
 import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.randomher0.ui.main.SectionsPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 //import com.randomher0.databinding.ActivityMain2Binding;
 
 public class CameraActivity extends AppCompatActivity {
-
-//    private ActivityMain2Binding binding;
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//
-//        binding = ActivityMain2Binding.inflate(getLayoutInflater());
-//        setContentView(binding.getRoot());
-//
-//        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-//        ViewPager viewPager = binding.viewPager;
-//        viewPager.setAdapter(sectionsPagerAdapter);
-//        TabLayout tabs = binding.tabs;
-//        tabs.setupWithViewPager(viewPager);
-//        FloatingActionButton fab = binding.fab;
-//
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-//    }
-
-//    private void readParams() {
-//        SharedPreferences sharedPref = getSharedPreferences(
-////                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
-////        SharedPreferences.Editor.getString("ssid");
-////        SharedPreferences.Editor.getString("password");
-////        SharedPreferences.Editor.getInt()
-////        SharedPreferences.Editor.getBoolean()
-//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,7 +69,118 @@ public class CameraActivity extends AppCompatActivity {
         videoView.start();
     }
 
-//    private void createCameraPreview() {
+
+    public void connectWifi(View view) {
+        //connectCam1();
+        connectCam2();
+    }
+
+    public void connectCam1() {
+//        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+//        String wifiSsid = sharedPreferences.getString("wifi_ssid", "Action one_5656_5G");
+//        String wifiPassword = sharedPreferences.getString("wifi_password", "12345678");
 //
-//    }
+//        final WifiNetworkSuggestion suggestion1 =
+//                new WifiNetworkSuggestion.Builder()
+//                        .setSsid(wifiSsid)
+//                        .setIsAppInteractionRequired(true) // Optional (Needs location permission)
+//                        .build();
+//
+//        final WifiNetworkSuggestion suggestion2 =
+//                new WifiNetworkSuggestion.Builder()
+//                        .setSsid("test2")
+//                        .setIsAppInteractionRequired(true) // Optional (Needs location permission)
+//                        .build();
+//
+//        final WifiNetworkSuggestion suggestion3 =
+//                new WifiNetworkSuggestion.Builder()
+//                        .setSsid("test3")
+//                        .setIsAppInteractionRequired(true) // Optional (Needs location permission)
+//                        .build();
+//
+//
+//        final List<WifiNetworkSuggestion> suggestionsList = new ArrayList<>();
+//        suggestionsList.add(suggestion1);
+//        suggestionsList.add(suggestion2);
+//        suggestionsList.add(suggestion3);
+//
+//        WifiManager wifiManager = (WifiManager)getSystemService(WIFI_SERVICE);
+//        final int status = wifiManager.addNetworkSuggestions(suggestionsList);
+//        if (status != WifiManager.STATUS_NETWORK_SUGGESTIONS_SUCCESS) {
+//            //TODO:
+//            // do error handling here…
+//        }
+//
+//        // Optional (Wait for post connection broadcast to one of your suggestions)
+//        final IntentFilter intentFilter = new IntentFilter(WifiManager.ACTION_WIFI_NETWORK_SUGGESTION_POST_CONNECTION);
+//
+//        final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+//            @Override
+//            public void onReceive(Context context, Intent intent) {
+//                if (!intent.getAction().equals(
+//                        WifiManager.ACTION_WIFI_NETWORK_SUGGESTION_POST_CONNECTION)) {
+//                    return;
+//                }
+//                //TODO:
+//                // do post connect processing here...
+//            }
+//        };
+//        getApplicationContext().registerReceiver(broadcastReceiver, intentFilter);
+
+//        //remember current configuration
+//        //WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+//
+//
+////        String wifiSsid = sharedPreferences.getString("wifi_ssid", "Action one_5656_5G");
+////        String wifiPassword = sharedPreferences.getString("wifi_password", "12345678");
+//
+//        WifiConfiguration wifiConfiguration = new WifiConfiguration();
+//        wifiConfiguration.SSID = String.format("\"%s\"", wifiSsid);
+//        wifiConfiguration.preSharedKey = String.format("\"%s\"", wifiPassword);
+//
+//        int netId = wifiManager.addNetwork(wifiConfiguration);
+//        wifiManager.disconnect();
+//        wifiManager.enableNetwork(netId, true);
+//        wifiManager.reconnect();
+    }
+
+    public void connectCam2() {
+        Context context = getApplicationContext();
+
+        final NetworkSpecifier specifier =
+                new WifiNetworkSpecifier.Builder()
+                        .setSsidPattern(new PatternMatcher("Action one", PatternMatcher.PATTERN_PREFIX))
+                        .setBssid(MacAddress.fromString("08:E9:F6:16:E1:48"))
+                        .setWpa2Passphrase("12345678")
+                        .build();
+
+        final NetworkRequest request =
+                new NetworkRequest.Builder()
+                        .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+                        .removeCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                        .setNetworkSpecifier(specifier)
+                        .build();
+
+        final ConnectivityManager connectivityManager = (ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        final ConnectivityManager.NetworkCallback networkCallback = new ConnectivityManager.NetworkCallback() {
+
+            @Override
+            public void onAvailable(@NonNull Network network) {
+                super.onAvailable(network);
+                Toast.makeText(context, "network is available", Toast.LENGTH_LONG);
+            }
+
+            @Override
+            public void onUnavailable() {
+                super.onUnavailable();
+                Toast.makeText(context, "network is unavailable", Toast.LENGTH_LONG);
+            }
+        };
+        connectivityManager.requestNetwork(request, networkCallback, 15000);
+
+        // Release the request when done.
+        //connectivityManager.unregisterNetworkCallback(networkCallback);
+    }
 }
